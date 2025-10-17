d7569878aec38c3dc5feb28630bc4dbf0bc8de41
Zaman980805 <cloudarmor75@gmail.com>
2025-10-18 00:25:30 +0500

    second commit

--- app.py
+++ app.py
@@ -1,41 +1,53 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
-import requests, sqlite3, time
+from flask_mysqldb import MySQL # type: ignore
+import requests, time
 
 app = Flask(__name__)
 KEY = "0716289f2e856a66748b0ec8098ed8ec"
 
-def db():
-    con = sqlite3.connect("weather.db")
-    c = con.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS cache(city TEXT PRIMARY KEY, data TEXT, t INTEGER)")
-    c.execute("CREATE TABLE IF NOT EXISTS fav(id INTEGER PRIMARY KEY AUTOINCREMENT, city TEXT)")
-    con.commit()
-    con.close()
+ 
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''      
+app.config['MYSQL_DB'] = 'weather_db'     
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-db()
-
+mysql = MySQL(app)
+ 
 def wdata(city):
-    con = sqlite3.connect("weather.db")
-    c = con.cursor()
-    c.execute("SELECT data,t FROM cache WHERE city=?", (city,))
-    r = c.fetchone()
-    if r:
-        d, t = r
-        if time.time() - t < 600:
-            con.close()
-            return eval(d)
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT temp, weather, humidity, t FROM Cache WHERE city = %s", [city])
+    r = cur.fetchone()
+ 
+    if r and (time.time() - r['t'] < 600):
+        cur.close()
+        return {
+            "name": city,
+            "main": {"temp": r['temp'], "humidity": r['humidity']},
+            "weather": [{"description": r['weather']}]
+        }
 
+    
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={KEY}&units=metric"
     res = requests.get(url)
     if res.status_code == 200:
         data = res.json()
-        c.execute("REPLACE INTO cache(city,data,t) VALUES(?,?,?)", (city, str(data), int(time.time())))
-        con.commit()
-        con.close()
+        temp = data['main']['temp']
+        hum = data['main']['humidity']
+        weather_desc = data['weather'][0]['description']
+        t = int(time.time())
+
+         
+        cur.execute("REPLACE INTO Cache(city, temp, weather, humidity, t) VALUES(%s, %s, %s, %s, %s)",
+                    (city, temp, weather_desc, hum, t))
+        mysql.connection.commit()
+        cur.close()
         return data
-    con.close()
+
+    cur.close()
     return None
 
+ 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     info, msg = None, ''
@@ -45,13 +57,14 @@ def home():
             info = wdata(city)
             if not info:
                 msg = "city not found"
-    con = sqlite3.connect("weather.db")
-    c = con.cursor()
-    c.execute("SELECT city FROM fav")
-    favs = [x[0] for x in c.fetchall()]
-    con.close()
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT city FROM fav")
+    favs = [x['city'] for x in cur.fetchall()]
+    cur.close()
     return render_template('index.html', weather=info, error=msg, favs=favs)
 
+ 
 @app.route('/api/<city>')
 def api(city):
     d = wdata(city)
@@ -63,23 +76,22 @@ def api(city):
             "hum": d["main"]["humidity"]
         })
     return jsonify({"error": "not found"}), 404
-
+ 
 @app.route('/fav/add/<city>')
 def fadd(city):
-    con = sqlite3.connect("weather.db")
-    c = con.cursor()
-    c.execute("INSERT OR IGNORE INTO fav(city) VALUES(?)", (city,))
-    con.commit()
-    con.close()
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT IGNORE INTO fav(city) VALUES(%s)", [city])
+    mysql.connection.commit()
+    cur.close()
     return redirect(url_for('home'))
 
+ 
 @app.route('/fav/del/<city>')
 def fdel(city):
-    con = sqlite3.connect("weather.db")
-    c = con.cursor()
-    c.execute("DELETE FROM fav WHERE city=?", (city,))
-    con.commit()
-    con.close()
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM fav WHERE city = %s", [city])
+    mysql.connection.commit()
+    cur.close()
     return redirect(url_for('home'))
 
  
@@ -89,11 +101,10 @@ def city_weather(city):
     msg = ''
     if not info:
         msg = "City not found"
-    con = sqlite3.connect("weather.db")
-    c = con.cursor()
-    c.execute("SELECT city FROM fav")
-    favs = [x[0] for x in c.fetchall()]
-    con.close()
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT city FROM fav")
+    favs = [x['city'] for x in cur.fetchall()]
+    cur.close()
     return render_template('index.html', weather=info, error=msg, favs=favs)
 
 if __name__ == "__main__":
